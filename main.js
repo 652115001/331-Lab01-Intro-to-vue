3ded262e8a38ad6e3af96121a794fc06b77099e1
652115001 <Karanthaphong_aree@cmu.ac.th>
2024-06-25 20:45:05 +0700

    4.2

--- main.js
+++ main.js
@@ -5,11 +5,14 @@ createApp({
         const image = ref('./assets/images/socks_green.jpg')
         const productDetail = ref('This is boots')
         const productLink = ref('https://www.camt.cmu.ac.th/index.php/th/')
+        const inStock = ref(true)
         return{
             product,
             productDetail,
             image,
-            productLink
+            productLink,
+            inStock
+
         }
     }
 }).mount('#app')
