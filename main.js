09fca7b7fe26779f4c2863c91127b360ee4234b5
652115001 <Karanthaphong_aree@cmu.ac.th>
2024-06-25 19:58:04 +0700

    2.4

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { createApp, ref } = Vue
 createApp({
     setup(){
-        const product = ref('Socks')
+        const product = ref('Boots')
         return{
             product
         }
