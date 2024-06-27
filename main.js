43b10017b7fb24d746f5028f98e2ce6bacf8c7a7
652115001 <Karanthaphong_aree@cmu.ac.th>
2024-06-27 21:54:35 +0700

    10.5+10.6

--- main.js
+++ main.js
@@ -4,15 +4,34 @@ const app = createApp({
        const cart = ref([]);
        const premium = ref(true)
        const details = ref(["50% cotton","30% wool","20% plyster"])
-       function updateCart(id){
-        cart.value.push(id)
-       }
+       function removeFromCart(_id){
+        for (var i = 0; i < cart.value.length; i++) {
+            if(cart.value[i].id === _id){
+                if(--cart.value[i].amount === 0){
+                    cart.value.splice(i, 1);
+                }
+                return;
+            }
+        }
+    }
+       function updateCart(_id) {
+        for (var i = 0; i < cart.value.length; i++) {
+            if(cart.value[i].id === _id){
+                cart.value[i].amount++;
+                return;
+            }
+        }
+        cart.value.push({id:_id, amount: 1})
+        return;
+    }
      
         return {
       cart,
       premium,
       details,
-      updateCart
+      updateCart,
+      removeFromCart
+      
     };
   },
 })
