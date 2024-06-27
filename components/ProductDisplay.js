54a2a554f2d4a6683b9684d882e8a97d4b35a8ca
652115001 <Karanthaphong_aree@cmu.ac.th>
2024-06-27 11:29:06 +0700

    9.8

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,5 +1,5 @@
 const productDisplay = {
-    template:`
+  template: `
     <div class="product-display">
         <div class="product-container">
           <div class="product-image">
@@ -17,6 +17,7 @@ const productDisplay = {
                 <p v-else-if="inventory > 10">In Stock</p>
                 <p v-else-if="inventory > 0">Low Stock</p>
                 <p v-else>Out of Stock</p>
+        <p>Shipping: {{shipping}}</p>        
         <p v-if="onSale">The product is onSale</p>
         <p v-else>The product is not sale</p>
         <ul>
@@ -28,82 +29,92 @@ const productDisplay = {
         <p><span v-for ="size in sizes" >{{size}}</span></p>
         <button class="button" :disabled='!inStock'@click="addToCart":class="{disabledButton: !inStock}">Add to cart</button>
         <button class ="button" @click="toggleInStock">toggleStock</button>
+        
       </div>
      `,
-     props:{
-        premium: Boolean
-     },
-        setup(props){
-        const product = ref("Boots");
-        const brand = ref("SE 311");
-       
-        const productDetail = ref("This is boots");
-        const productLink = ref("https://www.camt.cmu.ac.th/index.php/th/");
-       
-        const inventory = ref(7);
-        const details = ref(["50% cotton", "30% wool", "20% polyester"]);
-        const variants = ref([
-          {
-            id: 2234,
-            color: "green",
-            image: "./assets/images/socks_green.jpg",
-            quantity: 50, onSale:true
-          },
-          {
-            id: 2235,
-            color: "blue",
-            image: "./assets/images/socks_blue.jpg",
-            quantity: 0, onsale:false
-          },
-        ]);
-        const selectedVariant = ref(0);
-        function updateVariant(index) {
-          selectedVariant.value = index;
+  props: {
+    premium: Boolean,
+  },
+  setup(props) {
+    const shipping = computed(()=>{
+        if (props.premium){
+            return 'Free'
+        } else {
+            return 30
         }
-        const sizes = ref(["S", "M", "L"]);
-        const cart = ref(0);
-        function addToCart() {
-          cart.value += 1;
-        }
-        function updateImage(varientImage) {
-          image.value = varientImage;
-        }
-        function toggleInStock() {
-          inStock.value = !inStock.value;
-        }
-        const title = computed(() => {
-          return brand.value + "" + product.value;
-        });
-        const onSaleTitle = computed(() => {
-            return brand.value + "" + product.value +"Is on sale";
-          });
-        const image = computed(() =>{
-            return variants.value[selectedVariant.value].image
-        })
-        const inStock = computed(() =>{
-            return variants.value[selectedVariant.value].quantity
-        })
-        const onSale = computed(() =>{
-            return variants.value[selectedVariant.value].onSale
-        })
-        return {
-          productDetail,
-          image,
-          productLink,
-          inStock,
-          inventory,
-          onSale,
-          details,
-          variants,
-          sizes,
-          cart,
-          title,
-          addToCart,
-          updateImage,
-          toggleInStock,
-          updateVariant,
-          onSaleTitle,
-          shipping
-        };
+    })
+    const product = ref("Boots");
+    const brand = ref("SE 311");
+
+    const productDetail = ref("This is boots");
+    const productLink = ref("https://www.camt.cmu.ac.th/index.php/th/");
+
+    const inventory = ref(7);
+    const details = ref(["50% cotton", "30% wool", "20% polyester"]);
+    const variants = ref([
+      {
+        id: 2234,
+        color: "green",
+        image: "./assets/images/socks_green.jpg",
+        quantity: 50,
+        onSale: true,
       },
-}
+      {
+        id: 2235,
+        color: "blue",
+        image: "./assets/images/socks_blue.jpg",
+        quantity: 0,
+        onsale: false,
+      },
+    ]);
+    const selectedVariant = ref(0);
+    function updateVariant(index) {
+      selectedVariant.value = index;
+    }
+    const sizes = ref(["S", "M", "L"]);
+    const cart = ref(0);
+    function addToCart() {
+      cart.value += 1;
+    }
+    function updateImage(varientImage) {
+      image.value = varientImage;
+    }
+    function toggleInStock() {
+      inStock.value = !inStock.value;
+    }
+    const title = computed(() => {
+      return brand.value + "" + product.value;
+    });
+    const onSaleTitle = computed(() => {
+      return brand.value + "" + product.value + "Is on sale";
+    });
+    const image = computed(() => {
+      return variants.value[selectedVariant.value].image;
+    });
+    const inStock = computed(() => {
+      return variants.value[selectedVariant.value].quantity;
+    });
+    const onSale = computed(() => {
+      return variants.value[selectedVariant.value].onSale;
+    });
+    return {
+      productDetail,
+      image,
+      productLink,
+      inStock,
+      inventory,
+      onSale,
+      details,
+      variants,
+      sizes,
+      cart,
+      title,
+      addToCart,
+      updateImage,
+      toggleInStock,
+      updateVariant,
+      onSaleTitle,
+      shipping,
+    };
+  },
+};
