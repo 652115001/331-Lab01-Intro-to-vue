00f3e348a10a883916ef783d6560b75dfc3748d4
652115001 <Karanthaphong_aree@cmu.ac.th>
2024-06-27 12:01:10 +0700

    9.10.1

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ const app = createApp({
   setup() {
        const cart = ref(0);
        const premium = ref(true)
+       const details = ref("This are the high price sock?");
         return {
       cart,
-      premium
+      premium,
+      details
     };
   },
 })
 app.component('product-display',productDisplay)
+app.component('product-details',productDetails)
 app.mount('#app')
